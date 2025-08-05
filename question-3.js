0964e061f7cd7bff17a564e1881900c52ea6fe0f
Anuna <anuaq20@gmail.com>
2025-08-05 14:19:06 +0700

    feat: fetch and filter long user names in question-3.js

--- question-3.js
+++ question-3.js
@@ -1,1 +1,7 @@
 // เริ่มเขียนโค้ดตรงนี้
+
+const resp = await fetch("https://jsonplaceholder.typicode.com/users")
+const users = await resp.json()
+const names = users.map(user => user.name)
+const longNames = names.filter(name => name.length > 17)
+console.log(longNames)
